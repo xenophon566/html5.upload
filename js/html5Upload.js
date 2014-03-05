1d154439d80f5e69bec29ec352a8ee48c5d743d3
ShawnWu <xenophon566@gmail.com>
2014-03-05 15:29:14 +0800

    20140305_by_ShawnWu setParas modify
    setParas object - Deep Merge

--- js/html5Upload.js
+++ js/html5Upload.js
@@ -126,7 +126,7 @@ var html5 = (function(){
 	return {
 		paras : function(parameter){ return eval(parameter) },
 		
-		setParas : function(obj){ object = jQuery.extend(object, obj) },
+		setParas : function(obj){ object = jQuery.extend(true, object, obj) },
 		
 		progress : function(file){ return uploadProgress(file) },
 		
